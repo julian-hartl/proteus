f33ec2e813ed54c04453aa5b53e9c8bfa0021368
Julian Hartl <julian.hartl.dev@gmail.com>
2023-01-04 17:28:40 +0100

    fix build error

--- proteus-compiler-kotlin/src/main/kotlin/lang/proteus/api/Compilation.kt
+++ proteus-compiler-kotlin/src/main/kotlin/lang/proteus/api/Compilation.kt
@@ -1,9 +1,12 @@
 package lang.proteus.api
 
+import lang.proteus.diagnostics.MutableDiagnostics
+import lang.proteus.evaluator.EvaluationResult
 import lang.proteus.syntax.parser.SyntaxTree
 
 class Compilation(val syntaxTree: SyntaxTree) {
     fun evaluate(): EvaluationResult {
 
+        return EvaluationResult(MutableDiagnostics())
     }
 }
