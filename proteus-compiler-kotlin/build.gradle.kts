a3c879dd99f513bff77093e22e183b407ab55433
Julian Hartl <julian.hartl.dev@gmail.com>
2023-01-04 17:16:08 +0100

    update package uri

--- proteus-compiler-kotlin/build.gradle.kts
+++ proteus-compiler-kotlin/build.gradle.kts
@@ -17,7 +17,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/julian-hartl/proteus-lang")
+            url = uri("https://maven.pkg.github.com/julian-hartl/proteus")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
