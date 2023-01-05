ed5f27ceb4c2684d4e6bdaaf1f6f6c799df29301
Julian Hartl <julian.hartl.dev@gmail.com>
2023-01-05 13:43:29 +0100

    add unary precedence tests

--- proteus-compiler-kotlin/src/test/kotlin/lang/proteus/syntax/parser/ParserTest.kt
+++ proteus-compiler-kotlin/src/test/kotlin/lang/proteus/syntax/parser/ParserTest.kt
@@ -17,7 +17,7 @@ class ParserTest {
         val precedence2 = op2.precedence
         val text = "a ${op1.literal} b ${op2.literal} c"
         val expression = SyntaxTree.parse(text)
-        if(expression.hasErrors()) return;
+        if (expression.hasErrors()) return;
 
         val e = AssertingEnumerator.fromExpression(expression.root)
         if (precedence1 >= precedence2) {
@@ -49,11 +49,56 @@ class ParserTest {
 
     }
 
+    @ParameterizedTest(name = "Precedence: a {0} b {1} c")
+    @MethodSource
+    fun `parser UnaryExpression honors precedences`(unaryOperator: Operator, binaryOperator: Operator) {
+        val unaryPrecedence = unaryOperator.precedence
+        val binaryPrecedence = binaryOperator.precedence
+        val text = "a ${unaryOperator.literal} b ${binaryOperator.literal}"
+        val expression = SyntaxTree.parse(text)
+        if (expression.hasErrors()) return;
+
+        val e = AssertingEnumerator.fromExpression(expression.root)
+        if (unaryPrecedence >= binaryPrecedence) {
+            e.assertExpression(BinaryExpressionSyntax::class)
+            e.assertExpression(UnaryExpressionSyntax::class)
+            e.assertToken(unaryOperator, unaryOperator.literal)
+            e.assertExpression(NameExpressionSyntax::class)
+            e.assertToken(Token.Identifier, "a")
+            e.assertToken(unaryOperator, unaryOperator.literal)
+            e.assertExpression(NameExpressionSyntax::class)
+            e.assertToken(Token.Identifier, "b")
+            e.dispose()
+
+        } else {
+            e.assertExpression(UnaryExpressionSyntax::class)
+            e.assertToken(unaryOperator, unaryOperator.literal)
+            e.assertExpression(BinaryExpressionSyntax::class)
+            e.assertExpression(NameExpressionSyntax::class)
+            e.assertToken(Token.Identifier, "a")
+            e.assertExpression(NameExpressionSyntax::class)
+            e.assertToken(Token.Identifier, "b")
+            e.dispose()
+        }
+
+    }
+
     companion object {
         @JvmStatic
         fun `parser BinaryExpression honors precedences`(): Stream<Arguments> {
             val pairs = mutableListOf<Arguments>()
             for (op1 in Operators.binaryOperators) {
+                for (op2 in Operators.binaryOperators) {
+                    pairs.add(Arguments.of(op1, op2))
+                }
+            }
+            return pairs.stream()
+        }
+
+        @JvmStatic
+        fun `parser UnaryExpression honors precedences`(): Stream<Arguments> {
+            val pairs = mutableListOf<Arguments>()
+            for (op1 in Operators.unaryOperators) {
                 for (op2 in Operators.binaryOperators) {
                     pairs.add(Arguments.of(op1, op2))
                 }
